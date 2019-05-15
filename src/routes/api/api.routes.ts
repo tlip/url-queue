c8a158518f347425fe756b4b34b2775015e915b4
Thomas Lipari <thomas.lipari@gmail.com>
2019-05-15 11:26:10 -0700

    add definitions to * route in api.router

--- src/routes/api/api.routes.ts
+++ src/routes/api/api.routes.ts
@@ -9,7 +9,7 @@ import * as handlers from './api.handlers';
 export default express.Router()
   .get('/url/:url', handlers.url)
   .get('/result/:id', handlers.result)
-  .all('*', (req, res) => {
+  .all('*', (req: express.Request, res: express.Response) => {
     res
       .status(404)
       .end({ message: `'${req.url}' is 2fake4us.` });
